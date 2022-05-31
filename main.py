16eb0cc18276bcb8ee6443b1fb1b25fe03957f02
Kamal Singh <97425294+ksbring@users.noreply.github.com>
2022-05-30 22:55:49 -0500

    fix database connection

--- main.py
+++ main.py
@@ -69,6 +69,9 @@ class Comment(db.Model):
     blog_post = relationship("BlogPost", back_populates="comments")
 
 
+db.create_all()
+
+
 def admin_only(func):
     @wraps(func)
     def inner(*args, **kwargs):
